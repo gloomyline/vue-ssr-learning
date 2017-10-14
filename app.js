52fe17409a231705a442477698c8ab9e97f917a8
1211071880@qq.com <1211071880@qq.com>
2017-10-14 12:47:45 +0800

    using a page template

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 * @Author: AlanWang
 * @Date:   2017-10-14 00:01:59
 * @Last Modified by:   AlanWang
-* @Last Modified time: 2017-10-14 00:21:50
+* @Last Modified time: 2017-10-14 12:41:35
 */
+
+const fs = require('fs')
 
 // Rendering a Vue instance
 
@@ -14,7 +16,9 @@ const Vue = require('vue')
 // })
 
 // Create a renderer
-const renderer = require('vue-server-renderer').createRenderer()
+const renderer = require('vue-server-renderer').createRenderer({
+  template: fs.readFileSync('./index.template.html', 'utf-8')
+})
 
 // Render the Vue instance to HTML
 // renderer.renderToString(app, (err, html) => {
@@ -34,20 +38,20 @@ const server = require('express')()
 
 server.get('*', (req, res) => {
   const app = new Vue({
+    el: '#app',
     data: {
       url: req.url
     },
     template: '<div>The visited URL is: {{ url }}</div>'
   })
 
-  renderer.renderToString(app).then(html => {
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head><title>Hello</title></head>
-        <body>${html}</body>
-      </html>
-    `)
+  const context = {
+    title: 'Hello',
+    meta: `<meta charset="utf-8">`
+  }
+
+  renderer.renderToString(app, context).then(html => {
+    res.send(html)
   }).catch(err => {
     res.status(500).end('Interal Server Error')
     return
